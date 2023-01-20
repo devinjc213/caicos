58ddb5df422ef5a61fa07a84fb82a6587a79ec7a
devinjc213 <devinjc213@gmail.com>
2023-01-19 18:19:35 -0600

    utilize zod for req body validation, remove yarn

--- src/pages/api/sendMail.ts
+++ src/pages/api/sendMail.ts
@@ -1,22 +1,29 @@
 import nodemailer from 'nodemailer';
+import z from 'zod';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+const emailSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
+  checkInDate: z.string(),
+  checkOutDate: z.string(),
+  rentalLocation: z.string(),
+  phoneNumber: z.string(),
+})
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const { firstName, lastName, email, phoneNumber, checkInDate, checkOutDate, rentalLocation } = req.body;
+  const parseInput = emailSchema.safeParse(req.body);
 
   let userOptions = {};
   let ownerOptions = {};
-  if (typeof firstName === 'string'
-    && typeof lastName === 'string'
-    && typeof email === 'string'
-    && typeof checkInDate === 'string'
-    && typeof checkOutDate === 'string'
-    && typeof rentalLocation === 'string'
-    && typeof phoneNumber === 'string'
-  ) {
+
+  if (parseInput.success) {
+    const { firstName, lastName, email, phoneNumber, checkInDate, checkOutDate, rentalLocation } = parseInput.data;
+
     userOptions = {
       from: process.env.EMAIL_USER,
-      to: email,
+      to: parseInput.data.firstName,
       subject: `Your stay at ${rentalLocation} is confirmed!`,
       body: `${firstName} ${lastName} ${checkInDate} -> ${checkOutDate}`,
     }
@@ -27,6 +34,8 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       subject: `New booking at ${rentalLocation}`,
       body: `${firstName} ${lastName} ${email} ${phoneNumber} ${checkInDate} -> ${checkOutDate}`,
     }
+  } else {
+    return res.status(400);
   }
 
   const transporter = nodemailer.createTransport({
@@ -43,7 +52,7 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log('Email sent: ', info.response);
       return res.status(200);
     }
   });
@@ -52,8 +61,8 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
-      return res.status(200)
+      console.log('Email sent: ', info.response);
+      return res.status(200);
     }
   });
 }
